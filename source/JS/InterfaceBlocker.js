9b258e1fab76c0d0aabebaa893050b4cc50b3389
alexmartin9595 <smart193333@gmail.com>
2015-07-31 18:25:48 +0300

    added default params and corrected each method

--- source/JS/InterfaceBlocker.js
+++ source/JS/InterfaceBlocker.js
@@ -1,14 +1,20 @@
 (function ($) {
-    $.fn.block = function (text, delay) {
-        $(this).each(function () {            
-            var block = "<div class='additional-block'></div>";
+    $.fn.block = function (params) {
+		var defaultParams = {
+			text: "Wait, please...",
+			delay: 3000
+		}
+		var params = $.extend({}, defaultParams, params);
+		
+        this.each(function () {            
+            var block = "<div class='additional-block'></div>";			
             $(this).append(block);
             $('.additional-block')
                 .append("<img src='../../images/loader.gif'/>")
-                .append("<p>" + text + "</p>");
+                .append("<p>" + params.text + "</p>");
             setTimeout(function () {
                 $('.additional-block').remove();
-            }, delay);                
+            }, params.delay);                
         });          
     }
 })(jQuery);
--- source/JS/main.js
+++ source/JS/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-    $('button').click(function () {       
-        $('body').block("Wait, please...", 3000);
+    $('button').click(function () {		
+        $('body').block();
+    });
+	
+	var params = {
+		delay: 10000
+	}
+	
+	$('input').focus(function () {		
+        $('body').block(params);
     });
 });
